1bd6495baf62e58136ba06893f109e4f2df9ffa6
mathiasvacher <vachermathias42@gmail.com>
2026-07-19 23:53:50 +0200

    add cname

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),    
+    viteStaticCopy({
+        targets: [
+          {
+            src: 'CNAME', // Assurez-vous que CNAME est à la racine du projet
+            dest: '.' // Copie à la racine de dist
+          }
+        ]
+      })],
   base: "/",
 })
